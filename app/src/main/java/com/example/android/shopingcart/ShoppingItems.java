0d94a45d9fa6f7e82181c5f601986df7dc93d6d8
ahmed-razic <Ibrahim2012*>
2021-02-06 17:00:01 +0100

    added layouts for two activities

---
+++ app/src/main/java/com/example/android/shopingcart/ShoppingItems.java
@@ -0,0 +1,14 @@
+package com.example.android.shopingcart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_items);
+    }
+}
